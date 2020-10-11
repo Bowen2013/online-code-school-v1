57d62d194742156a0c0bcc76175a5647febfb22f
Bowen2013 <panbowen2011@gmail.com>
2020-10-10 20:36:09 -0700

    2 files modified

--- src/app/lessons/lesson_one/lesson-one.component.ts
+++ src/app/lessons/lesson_one/lesson-one.component.ts
@@ -5,13 +5,16 @@ import { CM_EXERCISE_OPTIONS, CM_VIEWONLY_OPTIONS } from "../constants";
 const INITIAL_EXERCISE_CODE =`function add(a, b) {
   // type your function here
 }`;
+const CORRECT_EXERCISE_CODE = `function add(a, b) {
+  return a + b;
+}`;
 
 @Component({
   selector: "lesson-one",
   templateUrl: "./lesson-one.component.html",
   styleUrls: ["./lesson-one.component.scss"]
 })
-export class LessonOneComponent implements AfterViewInit {
+export class LessonOneComponent {
   answerStatus = ANSWER_STATUS.UNSPECIFIED;
   exerciseOptions = CM_EXERCISE_OPTIONS;
   viewOnlyOptions = CM_VIEWONLY_OPTIONS;
@@ -22,8 +25,11 @@ export class LessonOneComponent implements AfterViewInit {
 }`;
 
   exerciseContent = INITIAL_EXERCISE_CODE;
+  actualAnswers = '';
+  correctAnswers = '';
+  testCases = [[1,2], [3,4], [-3, -4]];
+  testCasesString = '[1, 2], [3, 4], [-3, -4]';
 
-  ngAfterViewInit() {}
 
   get isCorrect(): boolean {
     return this.answerStatus === ANSWER_STATUS.CORRECT;
@@ -34,13 +40,31 @@ export class LessonOneComponent implements AfterViewInit {
   }
 
   run() {
-    const codeSnippet = this.exerciseContent + '\n add(1,2);';
-    const rst = eval(codeSnippet);
-    if (rst === 3) {
-      this.answerStatus = ANSWER_STATUS.CORRECT;
-    } else {
-      this.answerStatus = ANSWER_STATUS.WRONG;
+    this.clearPrevious();
+    let isWrong = false;
+    for(let i = 0; i < this.testCases.length; i++) {
+      const arg = this.testCases[i];
+      const correctAns = this.getResultFromFunctionString(CORRECT_EXERCISE_CODE, arg);
+      const actualAns = this.getResultFromFunctionString(this.exerciseContent, arg);
+      this.correctAnswers += `${correctAns}${i === this.testCases.length - 1 ? '' : ', '}`;
+      this.actualAnswers += `${actualAns}${i === this.testCases.length - 1 ? '' : ', '}`;
+      if (correctAns !== actualAns) {
+        isWrong = true;
+      }
     }
+    this.answerStatus = isWrong ? ANSWER_STATUS.WRONG : ANSWER_STATUS.CORRECT;
+  }
+
+  clearPrevious() {
+    this.answerStatus = ANSWER_STATUS.UNSPECIFIED;
+    this.actualAnswers = '';
+    this.correctAnswers = '';
+  }
+
+  getResultFromFunctionString(fnString, args) {
+    const wrapper = s => `{return ${fnString}}`;
+    const func = new Function(wrapper(fnString));
+    return func.call(null).apply(null, args);
   }
 
   reset() {
