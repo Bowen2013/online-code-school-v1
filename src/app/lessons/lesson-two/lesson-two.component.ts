bd0785a0749391b8ab4a1ada19d46465e19765ab
Bowen2013 <panbowen2011@gmail.com>
2020-10-30 11:55:25 -0700

    4 files modified

--- src/app/lessons/lesson-two/lesson-two.component.ts
+++ src/app/lessons/lesson-two/lesson-two.component.ts
@@ -1,7 +1,16 @@
 import { Component, EventEmitter, Output } from "@angular/core";
-import { CM_VIEWONLY_OPTIONS } from "../../../model/ui_constants";
+import {
+  CM_EXERCISE_OPTIONS,
+  CM_VIEWONLY_OPTIONS
+} from "../../../model/ui_constants";
+import { ANSWER_STATUS } from "../../../model/ui_model";
 
 const LESSON_TWO_INDEX = 2;
+const INITIAL_EXERCISE = `
+
+
+
+`;
 
 @Component({
   selector: "lesson-two",
@@ -11,21 +20,68 @@ const LESSON_TWO_INDEX = 2;
 export class LessonTwoComponent {
   @Output() exerciseFinish = new EventEmitter<number>();
 
+  myPetVariable = "${myPet}";
+
   viewOnlyOptions = CM_VIEWONLY_OPTIONS;
-  viewOnlyContent = `function add(a, b) {
-  // this is a view only code
-  // you can not edit it.
-}`;
+  exerciseOptions = CM_EXERCISE_OPTIONS;
+  answerStatus = ANSWER_STATUS.UNSPECIFIED;
+  errorMessage = "";
+
+  /** View only code mirror */
+  viewOnlyContent = `const myPet = 'armadillo';
+console.log(\`I own a pet \${myPet}.\`);
+// Output: I own a pet armadillo.`;
 
   /** Input for exercise */
-  exerciseContent = `function add(a, b) {
-  // type your function here
-}`;
-  solutionContent = `function add(a, b) {
-  return a + b;
-}`;
-  testCases = [[1, 2], [3, 4], [-3, -4]];
-  testCasesString = "[1, 2], [3, 4], [-3, -4]";
+  exerciseContent = INITIAL_EXERCISE;
+
+  solutionContent = "const myName = 'hello';const myCity = 'world';console.log(`My name is ${myName}. My favorite city is ${myCity}.`);";
+
+  /** Exercise related logic */
+  get isCorrect(): boolean {
+    return this.answerStatus === ANSWER_STATUS.CORRECT;
+  }
+
+  get isWrong(): boolean {
+    return this.answerStatus === ANSWER_STATUS.WRONG;
+  }
+
+  submit() {
+    this.clearPrevious();
+    if (
+      this.exerciseContent.indexOf("`") > 0 &&
+      this.exerciseContent.indexOf("console.log(") > 0 &&
+      this.exerciseContent.indexOf("`My name is ${myName}") > 0 &&
+      this.exerciseContent.indexOf("My favorite city is ${myCity}") > 0
+    ) {
+      this.answerStatus = ANSWER_STATUS.CORRECT;
+      this.onExerciseFinish();
+    } else {
+      this.answerStatus = ANSWER_STATUS.WRONG;
+      if (this.exerciseContent.indexOf("`") === -1) {
+        this.errorMessage = "Make sure to use ` symbol, not ' or \".";
+      } else if (this.exerciseContent.indexOf("console.log(") === -1) {
+        this.errorMessage = "Did you forgot using console.log()?";
+      } else if (this.exerciseContent.indexOf("${myName}") === -1) {
+        this.errorMessage = "Did you use ${myName} in the console.log()?";
+      } else if (this.exerciseContent.indexOf("${myCity}") === -1) {
+        this.errorMessage = "Did you use ${myCity} in the console.log()?";
+      } else {
+        this.errorMessage =
+          "Make sure the content is My name is NAME. My favorite city is CITY";
+      }
+    }
+  }
+
+  reset() {
+    this.clearPrevious();
+    this.exerciseContent = INITIAL_EXERCISE;
+  }
+
+  clearPrevious() {
+    this.answerStatus = ANSWER_STATUS.UNSPECIFIED;
+    this.errorMessage = "";
+  }
 
   onExerciseFinish() {
     // emit value to code school component
